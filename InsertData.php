0d07459050332e2c9a421e1cee109dfc86e8627f
mariemuh <151115595+mariears@users.noreply.github.com>
2024-05-20 02:44:08 +0800

    Add files via upload

---
+++ InsertData.php
@@ -0,0 +1,27 @@
+<?php 
+    include 'Koneksi.php';
+
+    $hasil = array();
+
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    $stb = $_POST["stb"];
+    $nama = $_POST["nama"];
+    $angkatan = $_POST["angkatan"];
+
+    $ps = $con->stmt_init();
+    $ps->prepare("insert into tb_mhs values (?, ?, ?)");
+    $ps->bind_param("ssi", $stb, $nama, $angkatan);
+    $ps->execute();
+
+    if ($ps->affected_rows > 0) {
+        $hasil['hasil'] = 1;
+    } 
+    else {
+        $hasil['hasil'] = 0;
+    }
+
+        $ps->close();
+        $con->close();
+
+        echo json_encode($hasil);
+?>
